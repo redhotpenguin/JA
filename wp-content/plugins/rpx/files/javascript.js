4af3f8d739807a2fb623206c13a501d2f2f33e79
Fred Moyer <fred@redhotpenguin.com>
2012-01-25 12:54:36 -0800

    Comment Flow

--- wp-content/plugins/rpx/files/javascript.js
+++ wp-content/plugins/rpx/files/javascript.js
@@ -1,6 +1,3 @@
-function showRPX(rpxelement){
-  document.getElementById(rpxelement).style.display = 'block';
-}
 
 function hideRPX(rpxelement){
   document.getElementById(rpxelement).style.display = 'none';
@@ -18,3 +15,18 @@ function setRPXuser(userlogin){
 function focusEmail(){
   document.getElementById('user_email').focus();
 }
+
+function showRPX(rpxelement, commentID){
+	document.getElementById(rpxelement).style.display = 'block';
+	if( commentID )
+		rpx_setCookie('anchor_to_comment', commentID, 1);
+	
+}
+
+function rpx_setCookie( c_name,value,exdays ){
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; ");
+	document.cookie=c_name + "=" + c_value;
+}
+
